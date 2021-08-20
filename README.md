96376a0dfa517fb64ce1c798e518f73fb221c787
maxarvid <max.arvid.anderson@gmail.com>
2021-08-20 16:16:53 +0200

    copy over BMI coursework

---
+++ README.md
@@ -0,0 +1,3 @@
+###BMI Calculator
+
+Based on the course materials, not the lecture.
---
+++ src/assets/js/bmiCalculator.js
@@ -0,0 +1,15 @@
+const calculateMetricBMI = (weight, height) => {
+  let result;
+  result = (weight / ((height / 100) * (height / 100))).toFixed(2);
+  return result;
+};
+
+const getBMIClassification = (value) => {
+  if (value < 18.5) {
+    return "Underweight";
+  } else if (value < 25) {
+    return "Healthy";
+  } else if (value < 30) {
+    return "Overweight";
+  }
+};
