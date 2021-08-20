3a33a0ea88bcfd5e6a816e5a36e5919a31a5a646
maxarvid <max.arvid.anderson@gmail.com>
2021-08-20 16:22:58 +0200

    change BMI calculation test to reflect new BMI method

--- cypress/integration/userCanTestTheirBMI.feature.js
+++ cypress/integration/userCanTestTheirBMI.feature.js
@@ -6,8 +6,8 @@ describe("User can check their BMI", () => {
     cy.get("#height").type("186");
     cy.get("#Calculate").click();
   });
-  it("displays a BMI value of 26.01", () => {
-    cy.get("#results").should("contain", "26.01");
+  it("displays a BMI value of 24.80", () => {
+    cy.get("#results").should("contain", "24.80");
   });
   it("displays a message of you are Overweight", () => {
     cy.get("#results").should("contain", "Overweight");
